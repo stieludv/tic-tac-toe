f95afee907d3f3b3f61c0e5794fef04ee18732fc
stieludv <19951864+stieludv@users.noreply.github.com>
2024-01-04 21:04:57 +0100

    Comments for factory functions to code

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+
+// Play round
+// (grid, marker (x or o), position)
+// addMarkerToPos(grid, marker, pos)
+// didSomeoneWin(grid)
+
+
+
+// Add marker to position (grid, marker, pos)
+
+
+
+// Did someone win? (grid)
+
+
+
+// Update display
