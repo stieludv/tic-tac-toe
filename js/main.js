c8dab0af45ba9cdcefdd716727a92496eb62b280
stieludv <19951864+stieludv@users.noreply.github.com>
2024-01-10 17:28:19 +0100

    generate board and return it for Gameboard function

--- js/main.js
+++ js/main.js
@@ -1,19 +1,55 @@
 
 
 
-// Play round
-// (grid, marker (x or o), position)
-// addMarkerToPos(grid, marker, pos)
-// didSomeoneWin(grid)
+// gameboard (height, width)
+// this returns the initial grid
+const Gameboard = (board = [], marker) => {
+
+    // Return a fresh board if no input is given
+    if (board == [] || typeof(board) !== typeof([])) {
+        return [
+            [""],
+            [""],
+            [""],
+            [""],
+            [""],
+            [""],
+            [""],
+            [""],
+            [""],
+        ];
+    }
+
+    // Add marker to position (grid, marker, pos)
+    const addMarker = (marker) => {
+        console.log(marker);
+    }  
+
+    if (typeof(marker) === typeof("")) {
+        if (marker.toLowerCase() === "x" || marker.toLowerCase() === "o") {
+            console.log(marker);
+        }
+    } 
+
+}
 
 
-
-// Add marker to position (grid, marker, pos)
-
+const Game = () => {
 
 
-// Did someone win? (grid)
+    // Did someone win? (grid) 
+    const checkForWin = () => {
+
+    }
+}
 
 
 
 // Update display
+
+
+
+// Play round
+// (grid, marker (x or o), position)
+// addMarkerToPos(grid, marker, pos)
+// didSomeoneWin(grid)
