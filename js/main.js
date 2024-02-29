35c0334ee19016fc36cc35faf1d84199bae4c6c1
stieludv <19951864+stieludv@users.noreply.github.com>
2024-02-29 18:14:21 +0100

    Score IIFE and Player factory function added

--- js/main.js
+++ js/main.js
@@ -3,45 +3,82 @@
 
 // gameboard (height, width)
 // this returns the initial grid
-const Gameboard = (board = [], marker) => {
+const Gameboard = (function () {
 
-    // Return a fresh board if no input is given
-    if (board == [] || typeof(board) !== typeof([])) {
-        return [
-            [""],
-            [""],
-            [""],
-            [""],
-            [""],
-            [""],
-            [""],
-            [""],
-            [""],
-        ];
+    // Return an array with nine items
+    return Array[9].fill("");
+
+
+    const checkForWin = () => {
+        // algorithm for checking win
+         
     }
 
     // Add marker to position (grid, marker, pos)
-    const addMarker = (marker) => {
+    const addMarker = (marker, pos) => {
         console.log(marker);
-    }  
+    }
 
     if (typeof(marker) === typeof("")) {
         if (marker.toLowerCase() === "x" || marker.toLowerCase() === "o") {
             console.log(marker);
         }
-    } 
+    }
 
-}
+})();
 
 
 const Game = () => {
+    const board = Gameboard();
 
+    // Take a turn
+    const takeTurn = () => {
+        
+    }
 
     // Did someone win? (grid) 
-    const checkForWin = () => {
+    const checkGameStatus = () => {
 
+        // Check for diagonal win
+        if (board[4] !== "") {
+            // It will be either X or O in the middle
+            // Check for diagonal wins
+        }
+
+        // Check wins in the rows and columns
+
+        // Have all positions been checked and none is empty anymore?
+        // Game draw?
+        
     }
 }
+
+const Player = ((name) => {
+
+    let name = name; 
+    let wins = 0;
+
+    win = () => {
+        wins++;
+    }
+
+    setName = (name) => {
+        name = name;
+    }
+})();
+
+const Score = (() => {
+    let round = 0;
+    let ties = 0;
+
+    next = () => {
+        round++;
+    }
+
+    tie = () => {
+        ties++;
+    }
+})
 
 
 
