de58d0de025e0f1b6a70198911268b258faecdef
stieludv <19951864+stieludv@users.noreply.github.com>
2024-03-03 05:46:16 +0100

    Refactored code (factory or IIFE?, name, internals) and basic logic has been laid out for testing such as tracking wins

--- js/main.js
+++ js/main.js
@@ -3,91 +3,184 @@
 
 // gameboard (height, width)
 // this returns the initial grid
-const Gameboard = (() => {
+const gameboard = (() => {
 
-    // Return an array with nine items
-    return Array[9].fill("");
-
-
-    const checkForWin = () => {
-        // algorithm for checking win
-         
-    }
+    // Create the gameboard array
+    const board = Array(9).fill("");
 
     // Add marker to position (grid, marker, pos)
     const addMarker = (marker, pos) => {
-        console.log(marker);
-    }
+        // Is it a legit move?
+        if (typeof(marker) === typeof("")) {
+            if (marker.toLowerCase() === "x" || marker.toLowerCase() === "o") {
+                console.log(marker);
+            }
+        }
+        else {
 
-    if (typeof(marker) === typeof("")) {
-        if (marker.toLowerCase() === "x" || marker.toLowerCase() === "o") {
-            console.log(marker);
         }
     }
 
+    const _verifyMatchingMarkers = (args) => {
+        // Returns true if all markers match
+        args.forEach(marker => {
+            // If any marker does not match the first one
+            if (marker !== args[0]) {
+                return false;
+            }
+        });
+
+        return true;
+    }
+
+    // Did someone win? (grid) 
+    const playerHasWon = () => {
+        // Returns an array [win(true?), marker(X or O?)]
+
+        // Check for diagonal win
+        if (board[4] !== "") {
+            // It will be either X or O in the middle
+            // Check for positive diagonal win
+            if (board[0] !== "" && board[8] !== "" && _verifyMatchingMarkers([board[0], board[4], board[8]])) {
+                return [true, board[4]];
+            }
+
+            // Check for negative diagonal win
+            if (board[0] !== "" && board[8] !== "" && _verifyMatchingMarkers([board[0], board[4], board[8]])) {
+                return [true, board[4]];
+            }
+        }
+
+        // Check wins in the rows and columns
+        // Create arrays of every possible row and column combo
+        // Check these arrays in _verifymatchingMarkers
+        // If anyone is true a Player has won
+
+        // Have all positions been checked and none is empty anymore?
+        // Game draw?
+        
+    }
+
+    const clear = () => {
+        board = Array(9).fill("");
+    }
+
+    return {
+        playerHasWon,
+        addMarker,
+        clear, 
+    }
 })();
 
 
-const Game = (() => {
-    const board = Gameboard();
+const gameController = (() => {
+
 
     // Take a turn
     const takeTurn = () => {
         
     }
 
-    // Did someone win? (grid) 
-    const checkGameStatus = () => {
 
-        // Check for diagonal win
-        if (board[4] !== "") {
-            // It will be either X or O in the middle
-            // Check for diagonal wins
-        }
 
-        // Check wins in the rows and columns
+    return {
 
-        // Have all positions been checked and none is empty anymore?
-        // Game draw?
-        
     }
 })();
 
 
-const Player = (name) => {
+const Player = (playerName) => {
+    // Players could keep track of their respective scores
+    // This way scores could be saved, leadersboards, etc
+    // Otherwise scoreController will only track and save scores across some game
+    // Players could change between games, but the score would not in scoreController
+    // Persistant storage?
 
-    let name = name; 
+    // Player does not have a "type".
+    // X or O is decided on "side" by gameController
+    // Thus a player could switch sides mid-game through gameController alone
+    // Game controller knows whos turn it is, and what turn they are.
+
+    let name = playerName;
+    // Tracks overall wins (both as X and O)
     let wins = 0;
 
-    win = () => {
+    // Could implement tracking of ties here too and respective wins as X and O
+
+    const winIncrement = () => {
         wins++;
     }
 
-    setName = (name) => {
+    const setName = (name) => {
         name = name;
+    }
+
+    const getName = () => {
+        return name;
+    }
+
+    const getWins = () => {
+        return wins;
+    }
+
+    return {
+        winIncrement,
+        setName,
+        getName,
+        getWins,
     }
 };
 
-const Score = (() => {
+
+const scoreController = (() => {
+    // Persistant storage?
+    // See Player for more info.
+
     let round = 0;
     let ties = 0;
+    let winX = 0;
+    let winO = 0;
 
-    next = () => {
+    const nextRound = () => {
         round++;
     }
 
-    tie = () => {
+    const tieIncrement = () => {
         ties++;
     }
-})
+
+    const resetScore = () => {
+        round = 0;
+        ties = 0;
+    }
+
+    const getScore = () => {
+        return {
+            "rounds": round,
+            "ties": ties
+        }
+    }
+
+    const winXIncrement = () => {
+        winX++;
+    }
 
 
+    const winOIncrement = () => {
+        winO++;
+    }
 
-// Update display
+    return {
+        nextRound,
+        tieIncrement,
+        resetScore,
+        winXIncrement,
+        winOIncrement
+    }
+})();
 
 
+const displayController = (() => {
+    // Update display
+})();
 
-// Play round
-// (grid, marker (x or o), position)
-// addMarkerToPos(grid, marker, pos)
-// didSomeoneWin(grid)
